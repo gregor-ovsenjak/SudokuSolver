2d993e7cc257c7077187765ab5f029aaaca31175
Gregor Ovsenjak <gregor.ovsenjak@gmail.com>
2022-10-14 22:15:43 +0200

    Added bold borders to every third row/cell. Prevented input of any characters that are not numbers on keyboard event.

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class BoardComponent implements OnInit {
   Rows: Array<number> = [1,2,3,4,5,6,7,8,9];
   Cols: Array<number> = [1,2,3,4,5,6,7,8,9];
+
+
+  ChooseBorderType(rowOrCol:number): string  {
+    let BorderBold: string = rowOrCol % 3 === 0 && rowOrCol != 9 ? "3px solid black": "1px solid #CCC";
+    return BorderBold;
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -8,13 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class CellComponent implements OnInit {
   
   color:string = "transparent";
-  
+
   changeColorOnEnter() {
     this.color = "rgba(54, 124, 255, 0.483)"
   }
+
   changeColorOnLeave() {
     this.color = "transparent"
   }
+
+  validate(evt: KeyboardEvent) {
+
+      // Handle paste
+     
+      let key = evt.keyCode || evt.which;
+      let key1 = String.fromCharCode(key);
+      
+      var regex = /[0-9]|\./;
+      if( !regex.test(key1) ) {
+        evt.returnValue = false;
+        if(evt.preventDefault) evt.preventDefault();
+      }
+  }
+
 
   constructor() { }
 
