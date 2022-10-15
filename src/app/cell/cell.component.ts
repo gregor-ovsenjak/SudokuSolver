ca5ee5c61b76d6f886deacd0ec687db29ba6d91e
Gregor Ovsenjak <gregor.ovsenjak@gmail.com>
2022-10-15 12:24:54 +0200

    Sudoku board takes only numbers from 0-9 as input.

--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -8,25 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class CellComponent implements OnInit {
   
   color:string = "transparent";
+  input:string = "";
+
 
   changeColorOnEnter() {
     this.color = "rgba(54, 124, 255, 0.483)"
   }
 
+  
   changeColorOnLeave() {
     this.color = "transparent"
   }
 
-  validate(evt: KeyboardEvent) {
 
-      // Handle paste
-     
-      let key = evt.keyCode || evt.which;
-      let key1 = String.fromCharCode(key);
+  validate(evt: KeyboardEvent) {
       
-      var regex = /[0-9]|\./;
-      if( !regex.test(key1) ) {
-        evt.returnValue = false;
+      let key: string  = evt.key;
+      var regex:RegExp = /[0-9]/;
+      
+      if( !regex.test(key) && evt.key !== "Backspace" ) {
         if(evt.preventDefault) evt.preventDefault();
       }
   }
